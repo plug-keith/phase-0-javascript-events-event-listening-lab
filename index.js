cf7a35d22bb08076ec1333ba6b990df684a6211f
plug_keith <keith.mwai@student.moringaschool.com>
2024-07-11 22:36:39 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function addingEventListener() {
-        const input = document.getElementById('button');
-        function clickAlert() {
-            alert('I was clicked!');
-        }
-        input.addEventListener('click', clickAlert);
-    }
-    addingEventListener(); 
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+
+}
